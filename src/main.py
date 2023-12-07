d716fa6501d5668ad1a81745df21efefb84da7b0
Brendan Shen <bshen.324@gmail.com>
2023-12-06 22:30:01 -0500

    add gravitation functions and test simulation

--- src/main.py
+++ src/main.py
@@ -15,8 +15,10 @@ dt = 0
 pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
-test_ball = GravityBall(10, 40, screen.get_width() / 2, screen.get_height() / 4, 0, 0, 0, G_0*FUDGE)
+# test_ball = GravityBall(10, 40, screen.get_width() / 2, screen.get_height() / 4, 0, 0, 0, G_0*FUDGE)
 # player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+test_sun = GravityBall(1e15, 40, screen.get_width() / 2, screen.get_height() / 2, 0, 0, 0, 0)
+test_planet = GravityBall(1e5, 20, screen.get_width() / 2 + 100, screen.get_height() / 2, 0, 10, 0, 0)
 
 while running:
     # poll for events
@@ -28,15 +30,24 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("cyan2")
     time = pygame.time.get_ticks() / 1000
-    timer_text = my_font.render(str(math.trunc(time)), False, (0, 0, 0))
-    dt_text = my_font.render(str(math.trunc(dt)), False, (0, 0, 0))
-    mechanical_energy_text = my_font.render(str(round(calculate_mechanical_energy(test_ball, HEIGHT), 2)), False, (0, 0, 0))
-    screen.blit(timer_text, (0,0))
-    screen.blit(mechanical_energy_text, (0,50))
-    screen.blit(dt_text, (0,100))
-    calculate_position(test_ball, dt)
-    collision_handler(test_ball, HEIGHT)
-    pygame.draw.circle(screen, "red", pygame.Vector2(test_ball.get_x(), test_ball.get_y()), test_ball.get_radius())
+    # timer_text = my_font.render(str(math.trunc(time)), False, (0, 0, 0))
+    # dt_text = my_font.render(str(math.trunc(dt)), False, (0, 0, 0))
+    # mechanical_energy_text = my_font.render(str(round(calculate_mechanical_energy(test_ball, HEIGHT), 2)), False, (0, 0, 0))
+    # screen.blit(timer_text, (0,0))
+    # screen.blit(mechanical_energy_text, (0,50))
+    # screen.blit(dt_text, (0,100))
+    # calculate_position(test_ball, dt)
+    # collision_handler(test_ball, HEIGHT)
+    # pygame.draw.circle(screen, "red", pygame.Vector2(test_ball.get_x(), test_ball.get_y()), test_ball.get_radius())
+    calculate_gravitational_force(test_sun, test_planet)
+    calculate_position(test_sun, dt)
+    calculate_position(test_planet, dt)
+    planet_x_text = my_font.render(str(test_planet.get_x()), False, (0, 0, 0))
+    screen.blit(planet_x_text, (0,0))
+    planet_y_text = my_font.render(str(test_planet.get_y()), False, (0, 0, 0))
+    screen.blit(planet_y_text, (0,50))
+    pygame.draw.circle(screen, "orange", pygame.Vector2(test_sun.get_x(), test_sun.get_y()), test_sun.get_radius())
+    pygame.draw.circle(screen, "blue", pygame.Vector2(test_planet.get_x(), test_planet.get_y()), test_planet.get_radius())
 
     # keys = pygame.key.get_pressed()
     # if keys[pygame.K_w]:
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 from objects import *
+import math
 # physics constants
 
 FUDGE = 25 # fudge factor to make animations more realistic
-G_0 = 9.80665
+G_0 = 9.80665 # acceleration due to gravity
+G = 6.67e-11 # gravitation constant
 
 # Environment variables
 LOSS = 0.5 # collisions will lose x% energy
@@ -14,6 +16,8 @@ def calculate_position(ball, dt):
     #ball.set_vy(ball.get_vy0() + time * ball.get_ay())
     # ball.set_x(ball.get_x0() + ball.get_vx0() * time + ball.get_ax() * time * time / 2)
     # ball.set_y(ball.get_y0() + ball.get_vy0() * time + ball.get_ay() * time * time / 2)
+    ball.set_vx(ball.get_vx() + dt * ball.get_ax())
+    ball.set_x(ball.get_x() + ball.get_vx() * dt + ball.get_ax() * dt * dt / 2)
     ball.set_vy(ball.get_vy() + dt * ball.get_ay())
     ball.set_y(ball.get_y() + ball.get_vy() * dt + ball.get_ay() * dt * dt / 2)
     return
@@ -26,3 +30,24 @@ def collision_handler(ball, height):
 
 def calculate_mechanical_energy(ball, height):
     return ball.get_mass() * ball.get_vy() * ball.get_vy() / 2 + (height - ball.get_y() - ball.get_radius()) * ball.get_mass() * G_0 * FUDGE
+
+def calculate_gravitational_force(ball, ball2):
+    m1 = ball.get_mass()
+    m2 = ball2.get_mass()
+    x1 = ball.get_x()
+    x2 = ball2.get_x()
+    y1 = ball.get_y()
+    y2 = ball2.get_y()
+    f = G * m1 * m2 / ((x2 - x1)**2 + (y2 - y1)**2)  # magnitude of the gravitational force
+    theta = math.atan2(y2 - y1, x2 - x1)  # angle between the line joining the two objects and the x-axis
+    fx = f * math.cos(theta)  # x component of the gravitational force
+    fy = f * math.sin(theta)  # y component of the gravitational force
+    ax = fx / ball.get_mass()
+    ay = fy / ball.get_mass()
+    ax2 = fx / ball2.get_mass()
+    ay2 = fy / ball2.get_mass()
+    ball.set_ax(ax if x1 < x2 else -ax)
+    ball.set_ay(ay if y1 < y2 else -ay)
+    ball2.set_ax(ax2 if x2 < x1 else -ax2)
+    ball2.set_ay(ay2 if y2 < y1 else -ay2)
+    return

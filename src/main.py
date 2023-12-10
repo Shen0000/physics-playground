9e662e7ff63e6f7175ecf11309995f74ca4c0a94
Brendan Shen <bshen.324@gmail.com>
2023-12-10 09:55:00 -0500

    add interactions between balls and vertical bounds

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
 # Example file showing a circle moving on screen
 import pygame
 import math
+import random
 from objects import *
 from utils import *
 
+
 # pygame setup
 pygame.init()
-WIDTH = 1280
-HEIGHT = 720
+WIDTH = 1280 * 2 - 100
+HEIGHT = 720 * 2 - 100
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+mode = 0 # code for the different modes: 0 is bouncing balls, 1 is gravity simulation
 pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 test_ball = GravityBall(10, 40, screen.get_width() / 2, screen.get_height() / 4, 0, 0, 0, G_0*FUDGE)
 # player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+bouncing_balls = []
+balls_colors = []
 
 while running:
     # poll for events
@@ -24,19 +29,51 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Get the mouse click position
+            x, y = pygame.mouse.get_pos()
+            collides = False
+            for ball in bouncing_balls:
+                if check_collision(ball, GravityBall(10, 40, x, y, 0, 0, 0, 0)):
+                    collides = True
+                    continue
+            if (not collides):
+                bouncing_balls.append(GravityBall(10, 40, x, y, random.randint(-100, 100), random.randint(-100, -50), 0, G_0*FUDGE))
+                balls_colors.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                screen.fill("cyan2")  # Fill the screen with cyan2 color
+                bouncing_balls.clear()
+            
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("cyan2")
-    time = pygame.time.get_ticks() / 1000
-    timer_text = my_font.render(str(math.trunc(time)), False, (0, 0, 0))
-    dt_text = my_font.render(str(math.trunc(dt)), False, (0, 0, 0))
-    mechanical_energy_text = my_font.render(str(round(calculate_mechanical_energy(test_ball, HEIGHT), 2)), False, (0, 0, 0))
-    screen.blit(timer_text, (0,0))
-    screen.blit(mechanical_energy_text, (0,50))
-    screen.blit(dt_text, (0,100))
-    calculate_position(test_ball, dt)
-    collision_handler(test_ball, HEIGHT)
-    pygame.draw.circle(screen, "red", pygame.Vector2(test_ball.get_x(), test_ball.get_y()), test_ball.get_radius())
+    # time = pygame.time.get_ticks() / 1000
+    # timer_text = my_font.render(str(math.trunc(time)), False, (0, 0, 0))
+    # dt_text = my_font.render(str(math.trunc(dt)), False, (0, 0, 0))
+    # mechanical_energy_text = my_font.render(str(round(calculate_mechanical_energy(test_ball, HEIGHT), 2)), False, (0, 0, 0))
+    # screen.blit(timer_text, (0,0))
+    # screen.blit(mechanical_energy_text, (0,50))
+    # screen.blit(dt_text, (0,100))
+    # calculate_position(test_ball, dt)
+    # collision_handler(test_ball, HEIGHT)
+    # pygame.draw.circle(screen, "red", pygame.Vector2(test_ball.get_x(), test_ball.get_y()), test_ball.get_radius())
+    if (mode == 0):
+        for i in range(len(bouncing_balls)):
+            ball = bouncing_balls[i]
+            color = balls_colors[i]
+            calculate_position(ball, 0.016)
+            collision_handler(ball, HEIGHT, WIDTH)
+            j = i - 1
+            while (j >= 0):
+                # if (ball.get_x() - bouncing_balls[j].get_x() > ball.get_radius() + bouncing_balls[j].get_radius()):
+                #     break
+                if (check_collision(ball, bouncing_balls[j])):
+                    ball_collision(ball, bouncing_balls[j])
+                j -= 1
+                    
+            pygame.draw.circle(screen, color, pygame.Vector2(ball.get_x(), ball.get_y()), ball.get_radius())
+            pygame.draw.circle(screen, (0, 0, 0), (ball.get_x(), ball.get_y()), ball.get_radius() + 1, width=2)
 
     # keys = pygame.key.get_pressed()
     # if keys[pygame.K_w]:
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 from objects import *
+import math
 # physics constants
 
 FUDGE = 25 # fudge factor to make animations more realistic
 G_0 = 9.80665
 
 # Environment variables
-LOSS = 0.5 # collisions will lose x% energy
+LOSS = 0.1 # collisions will lose x% momentum
 
 # physics functions
 
@@ -16,13 +17,69 @@ def calculate_position(ball, dt):
     # ball.set_y(ball.get_y0() + ball.get_vy0() * time + ball.get_ay() * time * time / 2)
     ball.set_vy(ball.get_vy() + dt * ball.get_ay())
     ball.set_y(ball.get_y() + ball.get_vy() * dt + ball.get_ay() * dt * dt / 2)
+    ball.set_vx(ball.get_vx() + dt * ball.get_ax())
+    ball.set_x(ball.get_x() + ball.get_vx() * dt + ball.get_ax() * dt * dt / 2)
     return
     
-def collision_handler(ball, height):
+def collision_handler(ball, height, width):
     if (ball.get_y() + ball.get_radius() >= height):
         ball.set_vy(-ball.get_vy() * (1-LOSS))
-        ball.set_y(height-ball.get_radius())
+        ball.set_vx(ball.get_vx() * (1-LOSS))
+        ball.set_y(height - ball.get_radius())
+    if (ball.get_x() + ball.get_radius() >= width):
+        ball.set_vy(ball.get_vy() * (1-LOSS))
+        ball.set_vx(-ball.get_vx() * (1-LOSS))
+        ball.set_x(width - ball.get_radius())
+    if (ball.get_x() - ball.get_radius() <=0):
+        ball.set_vy(ball.get_vy() * (1-LOSS))
+        ball.set_vx(-ball.get_vx() * (1-LOSS))
+        ball.set_x(ball.get_radius())
     return
 
 def calculate_mechanical_energy(ball, height):
     return ball.get_mass() * ball.get_vy() * ball.get_vy() / 2 + (height - ball.get_y() - ball.get_radius()) * ball.get_mass() * G_0 * FUDGE
+
+def check_collision(b, b2):
+    return (b.get_x() - b2.get_x()) ** 2 + (b.get_y() - b2.get_y()) ** 2 <= (b.get_radius() + b2.get_radius()) ** 2
+
+def ball_collision(b, b2):
+    dx = b2.get_x() - b.get_x()
+    dy = b2.get_y() - b.get_y()
+    distance = (dx ** 2 + dy ** 2) ** (1/2)
+    # Calculate the new velocities using the conservation of momentum and kinetic energy
+    angle = math.atan2(dy, dx)
+    m1 = b.get_mass()
+    m2 = b2.get_mass()
+    u1 = (b.get_vx() ** 2 + b.get_vy() ** 2) ** (1/2)
+    u2 = (b2.get_vx() ** 2 + b2.get_vy() ** 2) ** (1/2)
+    
+    # Velocity components along the line of impact
+    velocity_angle = math.atan2(b.get_vy(), b.get_vx())
+    velocity_angle2 = math.atan2(b2.get_vy(), b2.get_vx())
+    v1x = u1 * math.cos(velocity_angle - angle)
+    v1y = u1 * math.sin(velocity_angle - angle)
+    v2x = u2 * math.cos(velocity_angle2 - angle)
+    v2y = u2 * math.sin(velocity_angle2 - angle)
+    
+    # New velocities after the collision
+    v1x_prime = ((m1 - m2) * v1x + 2 * m2 * v2x) / (m1 + m2) * math.cos(angle) + u1 * math.sin(velocity_angle - angle) * math.cos(angle + math.pi/2)
+    v2x_prime = ((m2 - m1) * v2x + 2 * m1 * v1x) / (m1 + m2) * math.cos(angle) + u2 * math.sin(velocity_angle2 - angle) * math.cos(angle + math.pi/2)
+    v1y_prime = ((m1 - m2) * v1x + 2 * m2 * v2x) / (m1 + m2) * math.sin(angle) + u1 * math.sin(velocity_angle - angle) * math.sin(angle + math.pi/2)
+    v2y_prime = ((m2 - m1) * v2x + 2 * m1 * v1x) / (m1 + m2) * math.sin(angle) + u2 * math.sin(velocity_angle2 - angle) * math.sin(angle + math.pi/2)
+    
+    # Convert the velocities back to the original coordinate system
+    # v1x_prime = v1x_prime * math.cos(angle) + v1y * math.sin(angle)
+    # v1y_prime = v1x_prime * -math.sin(angle) + v1y * math.cos(angle)
+    # v2x_prime = v2x_prime * math.cos(angle) + v2y * math.sin(angle)
+    # v2y_prime = v2x_prime * -math.sin(angle) + v2y * math.cos(angle)
+    
+    # Update the velocities of the circles
+    # p1.velocity = Vector(v1x_prime, v1y_prime)
+    # p2.velocity = Vector(v2x_prime, v2y_prime)
+    b.set_vx(v1x_prime)
+    b.set_vy(v1y_prime)
+    b2.set_vx(v2x_prime)
+    b2.set_vy(v2y_prime)
+    # if (b2.y > b.y):
+    #     b.set_y(b2.y - distance * math.cos(angle))
+    #     b.set_x(b2.x - distance * math.sin(angle))
